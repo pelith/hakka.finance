245381a9ed351ba3ac6b2b5c0dbfa7f2d3e4f01f
paul-pelith <paul.li@pelith.com>
2026-01-13 17:56:58 +0800

    chore: update environment variables in .env.example, modify Netlify configuration for redirects and security headers, and enhance GitHub Actions workflows for deployment

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,11 +7,11 @@ interface ImportMetaEnv {
   readonly APP_BSC_NETWORK_URL: string;
   readonly APP_FANTOM_NETWORK_URL: string;
   readonly APP_POLYGON_NETWORK_URL: string;
-  readonly APP_FORTMATIC_KEY: string;
-  readonly APP_PORTIS_ID: string;
+  // readonly APP_FORTMATIC_KEY: string;
+  // readonly APP_PORTIS_ID: string;
   readonly APP_UAUTH_CLIENT_ID: string;
-  readonly APP_TAWK_ID: string;
-  readonly APP_TAWK_KEY: string;
+  // readonly APP_TAWK_ID: string;
+  // readonly APP_TAWK_KEY: string;
   readonly APP_PROJECT_GALAXY_ENDPOINT: string;
   readonly APP_WALLETCONNECT_PROJECT_ID: string;
 }
